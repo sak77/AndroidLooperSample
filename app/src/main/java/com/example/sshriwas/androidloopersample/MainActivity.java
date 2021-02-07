4cd87807a747661d29b9cc1ba1b69e2ee6f8cca6
Saket Shriwas <saket.shriwas@volvocars.com>
2021-02-07 10:30:04 +0100

    Update app to show RunOnUiThread, View.Post, PostDelayed, Handler.sendMessage and Handler.PostRunnable.

--- app/src/main/java/com/example/sshriwas/androidloopersample/MainActivity.java
+++ app/src/main/java/com/example/sshriwas/androidloopersample/MainActivity.java
@@ -1,116 +1,126 @@
 package com.example.sshriwas.androidloopersample;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /***
- * The purpose of this app is to explore some core Android Concepts.
- * Namely: Looper, MessageQueue, Messages, Handler & Messengers
- * We also want to see if we can communicate between threads using these concepts.
- * For this app we will start a thread. Add a MessageQueue via Looper, define
- * a handler to process the messages and later a messenger to maybe send
- * messages from different threads.
+ * To communicate between background thread and main thread in Android there are few options -
+ * - Activity.runOnUiThread()
+ * - View.post()
+ * - View.postDelayed()
+ * - Looper, MessageQueue, Messages/Runnables and Handler
+ * Purpose of this app is to demonstrate communication between threads using these concepts.
  */
 public class MainActivity extends AppCompatActivity {
     TextView txtOutput;
+    //Use Ctrl + J for templates
     private static final String TAG = "MainActivity";
-    private int mCount;
+    int optionSelected = -1;
+    RadioGroup radioGroup;
+    int sleepForMillis = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextInputEditText editText = findViewById(R.id.txtInput);
         txtOutput = findViewById(R.id.txtOutput);
-        Button btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(view -> {
-            //Take input value and pass it to thread
-            String input = editText.getText().toString();
-            ThreadUsingHandler myCalculateThread = new ThreadUsingHandler(Integer.parseInt(input));
-            //Thread.run() simply invokes code in run method on the thread.
-            //However Thread.start() creates a separate thread. So use start() to execute on
-            // background thread.
-            myCalculateThread.start();
+        Button btnSleep = findViewById(R.id.btnSleep);
+        btnSleep.setOnClickListener(view -> {
+            txtOutput.setText("Sleeping...");
+            //Thread.run() simply invokes code in run method on current thread.
+            //Thread.start() creates a separate thread.
+            //So use start() to execute on background thread.
+            switch (optionSelected) {
+                case R.id.rbRunOnUiThread:
+                    new ThreadUsingRunOnUiThread().start();
+                    break;
+                case R.id.rbViewPost:
+                    new ThreadUsingViewPost().start();
+                    break;
+                case R.id.rbViewPostDelayed:
+                    new ThreadUsingViewPostDelayed().start();
+                    break;
+                case R.id.rbHandlerSendMessage:
+                    new ThreadUsingHandler().start();
+                    break;
+                case R.id.rbHandlerPostRunnable:
+                    new ThreadUsingHandlerPostRunnable().start();
+                    break;
+                default:
+                    Log.d(TAG, "onCreate: Undefined id");
+            }
+        });
 
-            //new MyCalculateRunnable().run();
-
-            //CalculateThread2 calculateThread2 = new CalculateThread2(Integer.parseInt(input));
-            //calculateThread2.start();
+        radioGroup = findViewById(R.id.rgOption);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            btnSleep.setEnabled(true);
+            group.setEnabled(false);
+            optionSelected = checkedId;
         });
     }
 
     //Note - Just Implementing Runnable does not automatically cause its run() to be executed on
     // a separate thread. This is because Runnable is simply a functional interface with a single
-    // method run(). For background tasks, extend the Thread class only.
-    private class MyCalculateRunnable implements Runnable {
-
+    // method run(). For background tasks, extend the Thread class.
+    private class TestRunnable implements Runnable {
         @Override
         public void run() {
-
-            for (int i = 0; i < 10; i++) {
-                SystemClock.sleep(2000);
-                Log.i(TAG, "" + i);
-            }
-
         }
     }
 
-    /*
-    Instead of Handlers, this thread uses Activity.runOnUiThread() to communicate between worker
-    thread and the main thread.
-     */
     private class ThreadUsingRunOnUiThread extends Thread {
 
-        int mSeed;
-
-        ThreadUsingRunOnUiThread(int seed) {
-            mSeed = seed;
-        }
-
         @Override
         public void run() {
             super.run();
-
-            for (int i = 0; i < 10; i++) {
-                long value = myCalculateFunction(i);
-                Log.v(TAG, "Output - " + value);
-                MainActivity.this.runOnUiThread(() -> {
-                    txtOutput.setText("" + value);
-                    txtOutput.setBackgroundColor(
-                            Color.rgb(200, 250, value));
-                });
-                //SystemClock.sleep is similar to Thread.sleep() plus, you dont need the
-                //try catch block....
-                SystemClock.sleep(1000);
-            }
+            //SystemClock.sleep is similar to Thread.sleep() plus, you don't need the
+            //try catch block....
+            SystemClock.sleep(sleepForMillis);
+            MainActivity.this.runOnUiThread(MainActivity.this::wakeUp);
+            Log.d(TAG, "onCreate: R.id.rbRunOnUiThread");
         }
     }
 
-
-    /*
-    This thread uses Handler to communicate back to the UI thread.
-     */
-    private class ThreadUsingHandler extends Thread {
-        private int mSeed;
-
-        ThreadUsingHandler(int seed) {
-            mSeed = seed;
+    private class ThreadUsingViewPost extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            SystemClock.sleep(sleepForMillis);
+            txtOutput.post(MainActivity.this::wakeUp);
+            Log.d(TAG, "onCreate: R.id.rbViewPost");
         }
+    }
+
+    private class ThreadUsingViewPostDelayed extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            txtOutput.postDelayed(MainActivity.this::wakeUp, sleepForMillis);
+            Log.d(TAG, "onCreate: R.id.rbViewPostDelayed");
+        }
+    }
+
+    private void wakeUp() {
+        txtOutput.setText("Woke up!");
+        radioGroup.setEnabled(true);
+    }
+
+    private class ThreadUsingHandler extends Thread {
 
         @Override
         public void run() {
             super.run();
-
             /*
             The main components here are -
             1. MessageQueue - a queue that holds list of messages/runnables which are handled by the Handler.
@@ -124,30 +134,20 @@ public class MainActivity extends AppCompatActivity {
              */
 
             /*
-            Handler's default constructor takes no argument and it associates the handler with the
-            Looper of the current thread. But background threads do not create a message queue or Looper
-            by default. So before instantiating the default Handler instance, it is necessary to
+            Background threads do not create a message queue or Looper
+            by default. So before instantiating the Handler instance it is necessary to
             create a message queue and looper for the current thread. This is accomplished using
-            Looper.prepare(). So call this before you create the Handler instance. Also, after
-            Handler instance is created, call Looper.loop() to begin looping through the message queue.
+            Looper.prepare(). Also, after Handler instance is created, call Looper.loop()
+            to begin looping through the message queue.
 
             Get handler for Main Thread's Looper using Looper.getMainLooper().
+            Get handler for current thread's looper using Looper.myLooper().
 
             One more point to note here is that if the handler does not have reference to the main
             thread's looper then it CANNOT update the UI.
              */
+            Log.d(TAG, "onCreate: R.id.rbHandlerSendMessage");
 
-            /* Handler for main thread.....
-            Handler myHandler = new Handler(Looper.getMainLooper()) {
-                @Override
-                public void handleMessage(Message msg) {
-                    super.handleMessage(msg);
-                }
-            };*/
-
-            //Handler for current thread
-            try {
-                Looper.prepare();
                 /*
                 Anonymous inner class by default holds implicit reference to its parent class, which is
                 MainActivity in this case. That is why we are able to reference txtOutput inside it.
@@ -166,77 +166,109 @@ public class MainActivity extends AppCompatActivity {
 
                 /*
                 Update 04 Feb 2021 - Default Handler constructor new Handler() is deprecated.
-                Implicitly choosing a Looper during Handler construction can lead to bugs where operations
-                are silently lost (if the Handler is not expecting new tasks and quits), crashes
-                (if a handler is sometimes created on a thread without a Looper active), or race conditions,
-                where the thread a handler is associated with is not what the author anticipated.
-                Instead, use an Executor or specify the Looper explicitly, using Looper#getMainLooper, {link android.view.View#getHandler}, or similar.
-                If the implicit thread local behavior is required for compatibility,
-                 use new Handler(Looper.myLooper()) to make it clear to readers.
                  */
-                Handler myHandler = new Handler(Looper.myLooper()) {
+            Handler myHandler = new Handler(Looper.getMainLooper()) {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    wakeUp();
+                }
+
+                //Not sure whats difference between handle message and dispatch
+                //but its recommended to use handlemessage()
+                @Override
+                public void dispatchMessage(Message msg) {
+                    super.dispatchMessage(msg);
+                }
+            };
+            SystemClock.sleep(sleepForMillis);
+
+            /*
+            Instead of using the public constructor to define a message, better use
+            the handler.obtainMessage() to create message instance.
+
+            This is more efficient since it will try and recycle existing message instances
+            instead of creating a new instance every time.
+             */
+            //Message message = new Message();
+            Message message = myHandler.obtainMessage();
+            message.obj = "Done!";
+            //myHandler.sendMessage(message);
+            //We can also send empty message just to indicate that the task is completed.
+            myHandler.sendEmptyMessage(0);
+        }
+    }
+
+    private class ThreadUsingHandlerPostRunnable extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            //Since our handler is linked to MessageQueue of main thread. So we dont need to use
+            //Looper.prepare();
+            Log.d(TAG, "onCreate: R.id.rbHandlerPostRunnable");
+            SystemClock.sleep(sleepForMillis);
+            Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+            WakeUpRunnable myRunnable = new WakeUpRunnable();
+            mainThreadHandler.post(myRunnable);
+        }
+    }
+
+    private class WakeUpRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            wakeUp();
+        }
+    }
+
+
+    /*
+    Here i demonstrate how to create and link Handler to background thread...
+     */
+    private class ThreadUsingHandlerForBackgroundThreads extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            try {
+                //Since background threads do not have a MessageQueue and Looper by default,
+                //first use Looper.prepare() to setup the same
+                Looper.prepare();
+                //Instead of Looper.getMainLooper(), use Looper.myLooper() to link handler to
+                //MessageQUeue of current thread...
+                Handler myBackgroundHandler = new Handler(Looper.myLooper()) {
                     @Override
                     public void handleMessage(Message msg) {
                         super.handleMessage(msg);
-                        //Same as Thread.sleep() but without the try catch...
-                        SystemClock.sleep(3000);
-                        Log.v(TAG, "Handling message obj - " + msg.obj.toString());
+                        //This is handled on the message queue of the background thread...
+                        //below UI update will not work.
+                        //txtOutput.setBackgroundColor(Color.RED);
 
-                    /*
-                    This will work only if handler is associated with the main thread...
+                        //For some reason setText() method works from non-ui thread. But other UI updates require
+                        //handler that is created and linked to main thread's MessageQueue and Looper.
+                        //txtOutput.setText("Hello from background thread.");
 
-                        txtOutput.setText(msg.obj.toString());
-                        txtOutput.setBackgroundColor(
-                                Color.rgb(200, 250, Integer.parseInt(msg.obj.toString())));
-                    */
-                    }
-
-                    //Not sure whats difference between handle message and dispatch
-                    //but its recommended to use handlemessage()
-                    @Override
-                    public void dispatchMessage(Message msg) {
-                        super.dispatchMessage(msg);
+                        Log.d(TAG, "handleMessage: Got message " + msg.what);
                     }
                 };
 
-                for (int i = 0; i < 10; i++) {
-                    long value = myCalculateFunction(mSeed);
-
-                    /*
-                    Instead of using the public constructor to define a message, better use
-                    the handler.obtainMessage() to create message instance.
-
-                    This is more efficient since it will try and recycle existing message instances
-                    instead of creating a new instance every time.
-                     */
-                    //Message message = new Message();
-                    Message message = myHandler.obtainMessage();
-                    message.obj = "Message value " + value;
-                    Log.v(TAG, "Sending message obj - " + message.obj.toString());
-                    myHandler.sendMessage(message);
-                }
-
+                myBackgroundHandler.sendEmptyMessage(39);
+                //Start Looper to loop through MessageQueue
                 //The call to loop() doesn’t return;
                 // from the moment you call this method, your thread is processing messages until
                 // the quit() method is called on its Looper.
                 Looper.loop();
-
-            } finally {
+            }finally {
+                //Quit local looper safely
                 Looper.myLooper().quitSafely();
             }
         }
     }
 
-    private long myCalculateFunction(int seed) {
-        mCount+=4;
-        return (seed * mCount);
-    }
-
     /**
      * After this experiment my understanding about Loopers and Handlers is as follows-
      * The main benefit of the Looper and Handler class is to enable communication between
-     * the worker threads and the main UI thread. So after worker threads has completed their
-     * tasks they can update the UI thread by creating instance of Handler and passing reference
-     * of the main thread's looper via Looper.getMainLooper().
+     * the worker threads and the main UI thread. Worker threads can update the UI thread
+     * by creating instance of Handler and passing reference of the main thread's looper
+     * via Looper.getMainLooper().
      */
 }
